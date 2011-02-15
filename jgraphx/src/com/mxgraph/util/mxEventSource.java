fd649858feae6c944421512b535ac28a2141b76c
fabrizio morbini <fmorbini@gmail.com>
2011-02-15 20:16:43 +0000

    improved solution for issue 21 including support for headless mode (see suggestions in issue 21).

--- jgraphx/src/com/mxgraph/util/mxEventSource.java
+++ jgraphx/src/com/mxgraph/util/mxEventSource.java
@@ -148,7 +148,7 @@ public class mxEventSource
 	 */
 	public void fireEvent(mxEventObject evt)
 	{
-		//System.out.println("firing event "+evt.name+" "+evt.properties);
+		//System.out.println("firing event ("+this.getClass().getSimpleName()+")"+evt.name+" "+evt.properties);
 		//System.out.println(Arrays.asList(Thread.currentThread().getStackTrace()));
 		fireEvent(evt, null);
 	}
